98bb279ca5f57298f5c3dd16132d1940bc477b34
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-20 22:45:05 -0800

    add tests and code for a basic testing scenario

--- textUtilities.js
+++ textUtilities.js
@@ -3,7 +3,15 @@ var expect = require('chai').expect;
 expect(true).to.be.true;
 
 function titleCase (title) {
-	return title;
+  var words = title.split(' ');
+  var titleCasedWords = words.map(function (word){
+    return word[0].toUpperCase() + word.substring(1);
+  });
+  return titleCasedWords.join(' ');
+	// return title[0].toUpperCase() + title.substring(1);
 }
 
 expect(titleCase('the great mouse detective')).to.be.a('string');
+expect(titleCase('a')).to.equal('A');
+expect(titleCase('visitor')).to.equal('Visitor');
+expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
